fa397b3a15408bec4383de898af197fcdd0a8e3c
Ming-desu <joshuaming.jm@gmail.com>
2021-12-11 18:59:40 +0800

    fix auth

--- src/routes/api/api.route.js
+++ src/routes/api/api.route.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const Router = express.Router()
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../../config')
 
+Router.use('/auth', require('./api.auth.route'))
+
+Router.use((req, res, next) => {
+  try {
+    const bearer = req.header('Authorization')
+
+    if (!bearer) {
+      throw new Error('Token not found.')
+    }
+
+    const token = bearer.replace('Bearer ', '')
+    const decoded = jwt.verify(token, JWT_SECRET, { algorithms: ['HS256'] })
+  }
+  catch(err) {
+    return res.status(400).json({
+      errors: [err.message]
+    })
+  }
+
+  next()
+})
 Router.use('/drivers', require('./api.driver.route'))
 Router.use('/commuters', require('./api.commuter.route'))
 Router.use('/admins', require('./api.admin.route'))
-Router.use('/auth', require('./api.auth.route'))
 
 module.exports = Router

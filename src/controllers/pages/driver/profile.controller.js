cf77024306e7542ce43e86618d19674d9e40b69d
Ming-desu <joshuaming.jm@gmail.com>
2021-12-24 02:25:47 +0800

    added profile for driver

--- src/controllers/pages/driver/profile.controller.js
+++ src/controllers/pages/driver/profile.controller.js
@@ -1,4 +1,5 @@
 const { Request, Response } = require('express')
+const axios = require('axios')
 
 /**
  * Shows index page for driver profile
@@ -8,4 +9,40 @@ const { Request, Response } = require('express')
  */
 exports.index = function(req, res) {
   res.render('driver/profile/index.twig')
+}
+
+/**
+ * Updates commuter information to the database
+ * 
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+ exports.update = async function(req, res) {
+  try {
+    const { data: { message } } = await axios.patch('/api/drivers', req.body, {
+      headers: {
+        'Authorization': `Bearer ${req.cookies.token}`
+      }
+    })
+
+    res.cookie('r', '', {
+      maxAge: 1000 * 60 * 60 * 1,
+      httpOnly: true
+    })
+
+    res.json({
+      message
+    })
+  }
+  catch(err) {
+    let errors = [err.message]
+
+    if (err.response && err.response.data) {
+      errors = err.response.data.errors
+    }
+
+    res.status(400).json({
+      errors
+    })
+  }
 }
